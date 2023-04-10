3461b196c2e50d81b74fbded570db5b3721cfe28
airtonsena10 <airtonsena41@gmail.com>
2023-04-10 10:25:08 -0300

    ajustes

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,103 +1,127 @@
-import Pagination from "./Pagination"
-import Link from "next/link"
-import { useEffect, useState } from "react"
+import Pagination from "./Pagination";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
-export interface BlogPost{
-  userId: number,
-  id: number,
-  title: string,
-  body: string,
+export interface BlogPost {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
 }
 
-export interface comment{
-  postId: number,
-  id: number,
-  name: string,
-  email: string,
-  body: string,
+export interface comment {
+  postId: number;
+  id: number;
+  name: string;
+  email: string;
+  body: string;
 }
 
-function Posts(){
-  const [posts, setPosts] = useState<BlogPost[]>([])
-  const [currentPage, setCurrentPage] = useState(1)
-  const [totalPosts, setTotalPosts] = useState(0)
-  const [hasMore, setHasMore] = useState(true)
-  const postsPerPage = 6
-  const [user, setUser] = useState<any>()
+function Posts() {
+  const [posts, setPosts] = useState<BlogPost[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPosts, setTotalPosts] = useState(0);
+  const [hasMore, setHasMore] = useState(true);
+  const postsPerPage = 6;
+  const [user, setUser] = useState<any>();
 
   // Controla a paginação
   const nextPage = () => {
-    setCurrentPage(currentPage + 1)
-  }
+    setCurrentPage(currentPage + 1);
+  };
 
   const previousPage = () => {
-    if(currentPage > 1){
-      setCurrentPage(currentPage - 1)
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
     }
-  }
-
- 
-  useEffect(() => {
-    window.scrollTo({top: 0, behavior: 'smooth'})
-  }, [currentPage])
+  };
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.json())
-      .then(data => {
-        setPosts(data)
-        setTotalPosts(data.length)
-        setHasMore(data.length === postsPerPage)
-        const randomUserId = Math.floor(Math.random() * 10) + 1
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [currentPage]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => {
+        setPosts(data);
+        setTotalPosts(data.length);
+        setHasMore(data.length === postsPerPage);
+        const randomUserId = Math.floor(Math.random() * 10) + 1;
         fetch(`https://jsonplaceholder.typicode.com/users/${randomUserId}`)
-          .then(res => res.json())
-          .then(user => setUser(user))
-          .catch(error => console.error(error))
+          .then((res) => res.json())
+          .then((user) => setUser(user))
+          .catch((error) => console.error(error));
       })
-      .catch(error => console.error(`error fetching posts: ${error}`))
-  }, [])
+      .catch((error) => console.error(`error fetching posts: ${error}`));
+  }, []);
 
-  const indexOfLastPost = currentPage * postsPerPage
-  const indexOfFirstPost = indexOfLastPost - postsPerPage
-  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost)
-  const paginate = (pageNumber:number) => setCurrentPage(pageNumber)
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
 
-  return(<>
-    <div className="flex flex-col m-auto">
-    
-      <div id='logo' className="w-[100%] h-[100px] bg-[#1f1d1b] justify-center flex gap-[5rem] text-[1.3rem] pt-[.5rem] pb-[.5rem] shadow-[0_4px_6px_-1px_rgb(0,0,0,0.1),0_2px_4px_-2px_rgb(0,0,0,0.1)] font-semibold">
-       
-        <p className="text-[#d1d1ce] font-bold text-[1.5rem] uppercase">blog API</p>
-       
-      </div>
-      <div id='posts-section' className="w-[50%] m-auto max-[1359px]:w-[70%] max-[1000px]:w-[85%] max-[750px]:w-[100%]">
-     
-      {currentPosts.map(post => (
-        <div key={post.id} className="p-[2rem] m-[1rem] flex flex-col relative bg-[#363636]">
-          <div className="flex flex-col">
-            <p className="capitalize text-left mb-[1rem] text-[2rem]"><Link href={`/posts/${post.id}`}>{post.title}</Link></p>
-            <p className="mb-[1rem] font-semibold">Author: {user && user.name}</p>
-            <p className="text-[1.25rem] mb-[1rem]">{post.body}</p>
-          </div>
-        <div className="flex gap-4 absolute bottom-3 right-8">
-          <Link href={`/posts/${post.id}`} passHref>
-            Ler mais
-          </Link>
+  return (
+    <>
+      <div className=" flex flex-col items-center  ">
+        <div
+          id="logo"
+          className="border-r-emerald-50 rounded   w-[100%] h-[100px] bg-[#1f1d1b] justify-center flex gap-[5rem] text-[1.3rem] pt-[.5rem] pb-[.5rem] shadow-[0_4px_6px_-1px_rgb(0,0,0,0.1),0_2px_4px_-2px_rgb(0,0,0,0.1)] font-semibold"
+        >
+          <p className="text-[#d1d1ce] font-bold text-[1.5rem] uppercase">
+            blog API com tailwind
+          </p>
         </div>
+        <div
+          id="posts-section"
+          className="  grid grid-cols-3 gap-3 content-center"
+        >
+          {currentPosts.map((post) => (
+            <div
+              key={post.id}
+              className="rounded-md  p-[1rem] m-[1rem]   bg-[#D0D1E1]"
+            >
+              <div className="flex flex-col">
+                <p className="capitalize text-left mb-[1rem] text-[2rem]">
+                  <Link href={`/posts/${post.id}`}>{post.title}</Link>
+                </p>
+                <p className="mb-[1rem] font-semibold">
+                  Author: {user && user.name}
+                </p>
+                <p className="text-[1.2rem] mb-[1rem]">{post.body}</p>
+              </div>
+              
+            </div>
+            
+          ))}
         </div>
-      ))}
+
+        <div id="pagination" className="flex justify-center mt-[1rem]">
+          {currentPage === 1 ? null : (
+            <button
+              className="pr-[.5rem] text-[#ffffff] font-[500]"
+              onClick={previousPage}
+            >
+              Anterior
+            </button>
+          )}
+          <p className="font-bold text-[1.1rem] border-[1px] border-gray-500 rounded-[10px] w-[1.5rem] shadow-[0px_8px_24px_rgb(0,0,0,12%)]">
+            <span className="  text-[#D0D1E1] flex justify-center">{currentPage}</span>
+          </p>
+          <button
+            className="pl-[.5rem] text-[#D0D1E1] font-[500]"
+            onClick={nextPage}
+          >
+            Próximo
+          </button>
+        </div>
+        <div
+          id="footer"
+          className="w-full place-items-center bg-[#D0D1E1] items-center text-[1.3rem] mt-[2rem] pt-[.5rem] pb-[.5rem] font-semibold max-[750px]:w-full"
+        ></div>
       </div>
-      
-      <div id='pagination' className="flex justify-center mt-[1rem]">
-        {currentPage === 1 ? null : <button className='pr-[.5rem] text-[#686868] font-[500]' onClick={previousPage}>Anterior</button>}
-        <p className='font-bold text-[1.1rem] border-[1px] border-gray-500 rounded-[10px] w-[1.5rem] shadow-[0px_8px_24px_rgb(0,0,0,12%)]'><span className='flex justify-center'>{currentPage}</span></p>
-        <button className='pl-[.5rem] text-[#525252] font-[500]' onClick={nextPage}>Próximo</button>
-      </div>
-      <div id='footer' className="w-full place-items-center bg-[#525252] items-center text-[1.3rem] mt-[2rem] pt-[.5rem] pb-[.5rem] font-semibold max-[750px]:w-full">
-      
-      </div>
-    </div>
-  </>)
+    </>
+  );
 }
 
-export default Posts
+export default Posts;
--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,69 +1,104 @@
-import { useEffect, useState } from "react"
-import { useRouter } from "next/router"
-import { BlogPost, comment } from '../../components/Posts'
-import { HiOutlineArrowLeft } from 'react-icons/hi'
-import Link from "next/link"
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { BlogPost, comment } from "../../components/Posts";
+import { HiOutlineArrowLeft } from "react-icons/hi";
+import Link from "next/link";
 
-const PostDetails = () =>{
-  
-  const router = useRouter()
-  const { id } = router.query
+const PostDetails = () => {
+  const router = useRouter();
+  const { id } = router.query;
   console.log(router.query);
-  const [comments, setComments] = useState<comment[]>([])
-  const [post, setPost] = useState<BlogPost>({ userId: 0, id: 0, title: "", body: "" })
-  const [user, setUser] = useState<any>()
+  const [comments, setComments] = useState<comment[]>([]);
+  const [post, setPost] = useState<BlogPost>({
+    userId: 0,
+    id: 0,
+    title: "",
+    body: "",
+  });
+  const [user, setUser] = useState<any>();
 
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
-      .then(res => res.json())
-      .then(data => {
-        setPost(data)
-        
-        const randomUserId = Math.floor(Math.random() * 10) + 1
+      .then((res) => res.json())
+      .then((data) => {
+        setPost(data);
+
+        const randomUserId = Math.floor(Math.random() * 10) + 1;
         fetch(`https://jsonplaceholder.typicode.com/users/${randomUserId}`)
-          .then(res => res.json())
-          .then(user => setUser(user))
-          .catch(error => console.error(error))
+          .then((res) => res.json())
+          .then((user) => setUser(user))
+          .catch((error) => console.error(error));
       })
-      .catch(error => console.error(error))
-  }, [id])
+      .catch((error) => console.error(error));
+  }, [id]);
 
   useEffect(() => {
-    
     fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
-      .then(res => res.json())
-      .then(data => {
-        setComments(data)})
-      .catch(error => console.error(`error loading comments: ${error}`))
-  }, [])
-  
-  return(<>
-    <div id='main-div' className="w-[60%] m-auto max-[750px]:w-full">
-      <div className="w-[70%] m-auto pt-[1rem] pb-[2rem]">
-        <p className=""><Link href='/' className="hover:text-[blue]"><span className=""><HiOutlineArrowLeft/></span>Voltar</Link></p>
-      </div>
-      <div id='blog-post' className="flex flex-col items-center">
-        <div id='header' className="w-[75%] max-[750px]:w-[90%]">
-          <p className="text-[#8d8d8d] text-[2rem] capitalize text-center m-auto">{post.title}</p>
-        </div>
-          <p className="mt-[1rem]">Post by: {user && user.name}</p>
-        <div id='body' className="w-[60%] pt-[1rem] pb-[1rem] text-justify text-[1.3rem] max-[750px]:w-[90%]">
-          <p className="">{post.body}</p>
-        </div>
-        <div id='comments' className="w-[60%] max-[750px]:w-[100%]">
-          <div className="mt-[1rem] mb-[1rem] h-[.5px] bg-[#a1a1a1] w-[50%] text-[#7c7c7c] m-auto"></div>
-          <p className="font-bold text-center pt-[1rem]">Comentários:</p>
-          {comments.map((comment, index) => (
-            <div key={comment.id} className={`p-4 flex flex-col gap-1 ${index % 2 === 0 ? 'bg-[#242422]' : 'bg-[#4d4d4c]'}`}>
-              <p><span className="font-semibold text-[#0c0c0c]">Nome:</span> {comment.name}</p>
-              <p><span className='font-semibold text-[#3b3737]'>Email:</span> {comment.email}</p>
-              <p><span className='font-semibold text-[#000000]'>Mensagem:</span> {comment.body}</p>
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
-  </>)
-}
+      .then((res) => res.json())
+      .then((data) => {
+        setComments(data);
+      })
+      .catch((error) => console.error(`error loading comments: ${error}`));
+  }, []);
 
-export default PostDetails
+  return (
+    <>
+      <div id="main-div" className="w-[60%] m-auto max-[750px]:w-full">
+        <div className="w-[70%] m-auto pt-[1rem] pb-[2rem]">
+          <p className="">
+            <Link href="/" className="hover:text-[blue]">
+              <span className="">
+                <HiOutlineArrowLeft />
+              </span>
+              Voltar
+            </Link>
+          </p>
+        </div>
+        <div id="blog-post" className=" bg-[#D0D1E1] flex flex-col items-center rounded-md">
+          <div id="header" className="w-[75%] max-[750px]:w-[90%]">
+            <p className="text-[#181717] text-[2rem] capitalize text-center m-auto">
+              {post.title}
+            </p>
+          </div>
+          <p className="mt-[1rem]">Post by: {user && user.name}</p>
+          <div
+            id="body"
+            className="   w-[60%] pt-[1rem] pb-[1rem] text-justify text-[1.3rem] max-[750px]:w-[90%]"
+          >
+            <p className="">{post.body}</p>
+          </div>
+          </div>
+          <div id="comments" className=" flex flex-col items-center " >
+            <div className="mt-[2rem] mb-[2rem]  bg-[#3c4345]  text-[#7c7c7c] m-auto"></div>
+            <p className="  w-[100%]  font-bold text-center pt-[1rem]">Comentários:</p>
+            {comments.map((comment, index) => (
+              <div
+                key={comment.id}
+                className={`p-4 flex flex-col gap-2 rounded-md border-spacing-9 ${
+                  index % 2 === 0 ? "bg-[#131313]" : "bg-[#1a1919]"
+                }`}
+              >
+                <p>
+                  <span className="font-semibold text-[#adadad]">Nome:</span>{" "}
+                  {comment.name}
+                </p>
+                <p>
+                  <span className="font-semibold text-[#d6d4d4]">Email:</span>{" "}
+                  {comment.email}
+                </p>
+                <p>
+                  <span className="font-semibold text-[#c5c3c3]">
+                    Mensagem:
+                  </span>{" "}
+                  {comment.body}
+                </p>
+              </div>
+            ))}
+          </div>
+        
+      </div>
+    </>
+  );
+};
+
+export default PostDetails;
